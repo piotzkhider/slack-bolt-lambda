bc44199e7355afb9173d356331a93e907e12fe9f
piotzkhider <piotzkhider@gmail.com>
2019-10-03 06:33:18 +0900

    fix eslint

--- hello-world/lambda.js
+++ hello-world/lambda.js
@@ -1,15 +1,10 @@
-'use strict';
-
 const { createServer, proxy } = require('aws-serverless-express');
-const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware');
 const app = require('./app');
-
-app.use(awsServerlessExpressMiddleware.eventContext());
 
 exports.handler = (event, context, callback) => {
   const server = createServer(app);
 
-  context.succeed = (response) => {
+  context.succeed = response => {
     server.close();
     callback(null, response);
   };
